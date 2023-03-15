678ab1dbc8130602023e91f174f78ffa1699d002
Tiago Maia <gtiago.maia@gmail.com>
2023-03-15 19:07:28 +0000

    Update OpenWeatherMapAPI.kt

--- app/src/main/java/com/tiagomaia/weatherapp/network/OpenWeatherMapAPI.kt
+++ app/src/main/java/com/tiagomaia/weatherapp/network/OpenWeatherMapAPI.kt
@@ -3,5 +3,5 @@ package com.tiagomaia.weatherapp.network
 object OpenWeatherMapApi {
     // api key exposed only for test
     const val baseUrl = "https://api.openweathermap.org/data/2.5/"
-    const val WeatherPathQuery = "weather?id=524901&appid=8086a4ddd73f1ab35f7d951a89ef4205&units=metric"
+    const val WeatherPathQuery = "weather?id=524901&appid=<APP_ID>=metric"
 }
